07b08bab06876538dc2c9aa1f17edd5df66e2be9
Ovnuniarchos <pujalte.jorge@gmail.com>
2019-12-13 07:35:55 +0100

    Play controls.

--- main.gd
+++ main.gd
@@ -1,15 +1,13 @@
 extends Control
 
 """
-TODO:	Immediate note play skips notes (skips muted channels)
-		File selectors
-		Full play controls
-		Beautify file menu
+TODO:	Beautify file menu
 		Sound format selector
 		Export format selector
 		WAV writer error messages
 		File save/load errors
 		Make arpeggio last until new note?
+		Immediate note play skips notes (skips muted channels)
 """
 
 func _ready()->void:
@@ -22,12 +20,14 @@ func _on_tab_changed(tab:int)->void:
 
 func _on_PlayButton_toggled(button_pressed:bool)->void:
 	if button_pressed:
+		$PanelContainer/PanelContainer/HBoxContainer/PlayButton2.pressed=false
 		AUDIO.tracker.play(GLOBALS.curr_order)
 	else:
 		AUDIO.tracker.stop()
 
 func _on_PlayButton2_toggled(button_pressed):
 	if button_pressed:
+		$PanelContainer/PanelContainer/HBoxContainer/PlayButton.pressed=false
 		AUDIO.tracker.play_track(GLOBALS.curr_order)
 	else:
 		AUDIO.tracker.stop()
--- ui/file/file_tab.gd
+++ ui/file/file_tab.gd
@@ -3,25 +3,34 @@ extends Tabs
 const BUFFER_SIZE=2048
 
 
+enum FILE_MODE{LOAD,SAVE,SAVE_WAV}
+
+
+var file_mode:int
+
+
 func _on_New_pressed():
 	pass # Replace with function body.
 
 func _on_Save_pressed()->void:
+	file_mode=FILE_MODE.SAVE
+	$FileDialog.window_title="Save Song"
+	$FileDialog.mode=FileDialog.MODE_SAVE_FILE
 	$FileDialog.popup_centered_ratio()
-	return
-	var f:ChunkedFile=ChunkedFile.new()
-	f.open(".songs/tmp.txt",File.WRITE)
-	GLOBALS.song.serialize(f)
-	f.close()
 
 func _on_Load_pressed()->void:
-	var f:ChunkedFile=ChunkedFile.new()
-	f.open(".songs/tmp.txt",File.READ)
-	var new_song:Song=GLOBALS.song.deserialize(f)
-	f.close()
-	if new_song!=null:
-		AUDIO.tracker.stop()
-		GLOBALS.song=new_song
+	file_mode=FILE_MODE.LOAD
+	$FileDialog.window_title="Load Song"
+	$FileDialog.mode=FileDialog.MODE_OPEN_FILE
+	$FileDialog.popup_centered_ratio()
+
+func _on_SaveWav_pressed()->void:
+	file_mode=FILE_MODE.SAVE_WAV
+	$FileDialog.window_title="Export as Wave"
+	$FileDialog.mode=FileDialog.MODE_SAVE_FILE
+	$FileDialog.popup_centered_ratio()
+
+#
 
 func _on_CleanPats_pressed()->void:
 	AUDIO.tracker.stop()
@@ -37,15 +46,29 @@ func _on_CleanWaves_pressed():
 
 #
 
-func _on_SaveWav_pressed()->void:
-	var synth:Synth=Synth.new()
-	var tracker:Tracker=Tracker.new(synth)
-	var file:WaveFile=WaveFile.new()
-	var cmds:Array=[]
-	cmds.resize(65536)
-	synth.set_mix_rate(11025.0)
-	GLOBALS.song.sync_waves(synth)
-	file.start_file(".songs/z.wav",false,11025)
-	while tracker.gen_commands(GLOBALS.song,11025.0,BUFFER_SIZE,cmds):
-		file.write_chunk(synth.generate(BUFFER_SIZE,cmds,1.0))
-	file.end_file()
+func _on_file_selected(path:String)->void:
+	if file_mode==FILE_MODE.SAVE:
+		var f:ChunkedFile=ChunkedFile.new()
+		f.open(path,File.WRITE)
+		GLOBALS.song.serialize(f)
+		f.close()
+	elif file_mode==FILE_MODE.LOAD:
+		var f:ChunkedFile=ChunkedFile.new()
+		f.open(path,File.READ)
+		var new_song:Song=GLOBALS.song.deserialize(f)
+		f.close()
+		if new_song!=null:
+			AUDIO.tracker.stop()
+			GLOBALS.song=new_song
+	elif file_mode==FILE_MODE.SAVE_WAV:
+		var synth:Synth=Synth.new()
+		var tracker:Tracker=Tracker.new(synth)
+		var file:WaveFile=WaveFile.new()
+		var cmds:Array=[]
+		cmds.resize(65536)
+		synth.set_mix_rate(11025.0)
+		GLOBALS.song.sync_waves(synth)
+		file.start_file(path,false,11025)
+		while tracker.gen_commands(GLOBALS.song,11025.0,BUFFER_SIZE,cmds):
+			file.write_chunk(synth.generate(BUFFER_SIZE,cmds,1.0))
+		file.end_file()

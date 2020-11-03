191c2028a45b45475c430e43f7b38b406bfe85b6
Ovnuniarchos <pujalte.jorge@gmail.com>
2020-11-04 00:01:06 +0100

    Fixed: MIDI watchdog considers ALSA virtual MIDI devices real devices.

--- singletons/midi.gd
+++ singletons/midi.gd
@@ -1,34 +1,38 @@
 extends Node
 
-var watching:bool
 var midi_open:bool
-var thread:Thread
+var watchdog:Timer
 
 func _ready()->void:
-	watching=true
 	midi_open=false
-	thread=Thread.new()
-	thread.start(self,"_watchdog_thread",null)
+	watchdog=Timer.new()
+	watchdog.one_shot=false
+	watchdog.wait_time=0.02
+	watchdog.autostart=true
+	watchdog.connect("timeout",self,"_watchdog_thread")
+	add_child(watchdog)
 
 func _notification(n:int)->void:
 	if n==NOTIFICATION_PREDELETE:
-		watching=false
-		thread.wait_to_finish()
 		OS.close_midi_inputs()
 
-func _watchdog_thread(_args:Object)->void:
-	while watching:
-		if !midi_open:
-			OS.open_midi_inputs()
-			midi_open=true
-		var mdl:PoolStringArray=OS.get_connected_midi_inputs()
-		if mdl.empty():
-			midi_open=false
-		else:
-			for md in mdl:
-				if md.empty():
-					midi_open=false
-					break
-		if !midi_open:
-			OS.close_midi_inputs()
-		OS.delay_msec(25)
+func _watchdog_thread()->void:
+	if !midi_open:
+		OS.open_midi_inputs()
+		midi_open=true
+	var mdl:Array=[]
+	for md in OS.get_connected_midi_inputs():
+		if md.to_lower().find("virtual")==-1: # Ignore ALSA virtual devices
+			mdl.append(md)
+	if mdl.empty():
+		midi_open=false
+	else:
+		for md in mdl:
+			if md.empty():
+				midi_open=false
+				break
+	if !midi_open:
+		OS.close_midi_inputs()
+		DEBUG.set_var("MIDI","off")
+	else:
+		DEBUG.set_var("MIDI",String(mdl))

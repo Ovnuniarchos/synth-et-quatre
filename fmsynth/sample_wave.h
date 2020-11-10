cdcbb74f9c6e6f17bd8ddc85d2ac120ac1ca3a7a
Ovnuniarchos <pujalte.jorge@gmail.com>
2020-11-10 08:59:14 +0100

    Fixed: Wrong loop start/size calculation.

--- fmsynth/sample_wave.h
+++ fmsynth/sample_wave.h
@@ -34,14 +34,14 @@ private:
 public:
 	SampleWave(godot::Array sample_in,int loop_0,int loop_1,float rec_freq,float sam_freq){
 		size=sample_in.size();
-		loop_start=clamp(loop_start,(int64_t)0,size);
-		int64_t loop_end=clamp(loop_end,(int64_t)0,size);
+		int64_t loop_start=clamp((int64_t)loop_0,0L,size);
+		int64_t loop_end=clamp((int64_t)loop_1,0L,size);
 		if(loop_start>loop_end){
 			int64_t t=loop_start;
 			loop_start=loop_end;
 			loop_end=t;
 		}
-		loop_size=loop_end-loop_start;
+		loop_size=(loop_end-loop_start)+(loop_end==loop_start?0:1);
 		recorded_freq=rec_freq;
 		sample_freq=sam_freq;
 		if(size==0){
@@ -50,6 +50,7 @@ public:
 			sample=new FixedPointShort[size];
 			for(int i=0;i<size;i++) sample[i]=((float)sample_in[i])*FP_ONE;
 		}
+		// printf("%d (%d) %d\n",loop_start,loop_end,loop_size);
 	}
 
 	~SampleWave(){
@@ -57,7 +58,8 @@ public:
 	};
 
 	FixedPoint fix_loop(FixedPoint phi,FixedPoint mod_in) override{
-		return _fix_loop(phi,mod_in);
+		FixedPoint p2=_fix_loop(phi,mod_in);
+		return p2;
 	}
 
 	FixedPoint generate(FixedPoint phi,FixedPoint pm_in,FixedPoint duty_cycle) override{

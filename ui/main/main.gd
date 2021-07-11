d53a44f2bb329223d5138cbf2a65092c328d5baa
Ovnuniarchos <pujalte.jorge@gmail.com>
2021-07-11 17:43:42 +0200

    Play controls refactored.

--- ui/main/main.gd
+++ ui/main/main.gd
@@ -24,23 +24,8 @@ func _ready()->void:
 	ThemeHelper.apply_styles_group(theme,"LabelTitle","Title")
 	ThemeHelper.apply_styles_group(theme,"LabelControl","Label")
 
+
 func _on_tab_changed(tab:int)->void:
 	var t:Tabs=$Main/Tabs.get_tab_control(tab)
 	if t.has_method("update_ui"):
 		t.update_ui()
-
-
-func _on_PlayControls_play_all(on:bool)->void:
-	if on:
-		$PlayControls/PC/HBC/PlayTrack.pressed=false
-		AUDIO.tracker.play(GLOBALS.curr_order)
-	else:
-		AUDIO.tracker.stop()
-
-
-func _on_PlayControls_play_track(on:bool)->void:
-	if on:
-		$PlayControls/PC/HBC/Play.pressed=false
-		AUDIO.tracker.play_track(GLOBALS.curr_order)
-	else:
-		AUDIO.tracker.stop()
--- ui/play_controls/play_controls.gd
+++ ui/play_controls/play_controls.gd
@@ -1,14 +1,18 @@
 extends Control
 
-signal play_all(on)
-signal play_track(on)
+
+onready var play:Button=$HBC/Play
+onready var play_track:Button=$HBC/PlayTrack
+
 
 func _ready()->void:
 	GLOBALS.connect("song_changed",self,"_on_song_changed")
 
+
 func _on_song_changed()->void:
-	$PC/HBC/Play.pressed=false
-	$PC/HBC/PlayTrack.pressed=false
+	play.pressed=false
+	play_track.pressed=false
+
 
 func _unhandled_input(ev:InputEvent)->void:
 	if not(ev is InputEventKey):
@@ -19,12 +23,20 @@ func _unhandled_input(ev:InputEvent)->void:
 	if ev.pressed:
 		return
 	if ev.scancode==GKBD.PLAY_ALL:
-		$PC/HBC/Play.pressed=!$PC/HBC/Play.pressed
+		play.pressed=!play.pressed
 	else:
-		$PC/HBC/PlayTrack.pressed=!$PC/HBC/PlayTrack.pressed
+		play_track.pressed=!play_track.pressed
+
 
 func _on_Play_toggled(pressed:bool)->void:
-	emit_signal("play_all",pressed)
+	if pressed:
+		AUDIO.tracker.play(GLOBALS.curr_order)
+	else:
+		AUDIO.tracker.stop()
+
 
 func _on_PlayTrack_toggled(pressed:bool)->void:
-	emit_signal("play_track",pressed)
+	if pressed:
+		AUDIO.tracker.play_track(GLOBALS.curr_order)
+	else:
+		AUDIO.tracker.stop()

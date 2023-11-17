2d83b4ff4bda711162046d719e58315698634a05
Ovnuniarchos <pujalte.jorge@gmail.com>
2023-11-17 07:43:56 +0100

    FIXED: Arpeggio editor doesn't clear on song changes.

--- ui/arp_designer/arp_designer.gd
+++ ui/arp_designer/arp_designer.gd
@@ -7,19 +7,26 @@ onready var arp_edit=get_node("%Values")
 
 
 func _ready()->void:
+	GLOBALS.connect("song_changed",self,"_on_arp_selected",[-1])
 	_on_arp_selected(-1)
+
 
 func _on_arp_selected(index:int)->void:
 	arp=GLOBALS.song.get_arp(index)
 	GLOBALS.curr_arp=arp
-	arp_name.text=arp.name if arp!=null else ""
-	arp_edit.visible=arp!=null
 	if arp!=null:
+		arp_name.text=arp.name
+		arp_edit.visible=true
 		arp_edit.set_macro(arp)
+	else:
+		arp_name.text=""
+		arp_edit.visible=false
+
 
 func _on_Name_text_changed(txt:String)->void:
 	if arp!=null:
 		arp.name=txt
+
 
 func _on_Values_arp_changed(_parameter:String, values:Array, steps:int, loop_start:int, loop_end:int, release_loop_start:int, _relative:int, _tick_div:int, _delay:int):
 	if arp==null:
